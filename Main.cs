2a628f65bc8c303920a64ae91b741dea42d3ec80
mifegui <ericmfguimaraes@tuta.io>
2020-06-05 17:52:52 -0300

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Wox.Plugin;
+using System.IO;
+
+namespace Appendisso
+{
+    public class Main : IPlugin
+    {
+        string fileToAppendPath = "c:\\Users\\Example.txt";
+
+
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            // Get text typed by user
+            string text = query.Search;
+
+            var result = new Result
+            {
+                Title = "Append to file",
+                SubTitle = fileToAppendPath + " " + text,
+                IcoPath = "Images\\appendisso.png",
+                Action = (Func<ActionContext, bool>)(c =>
+               {
+                   // When the user hits enter, run the method
+                   Append(text);
+                   // And hide the search bar
+                   return true;
+               })
+            };
+
+            return new List<Result> { result };
+        }
+
+        public void Append(string text)
+        {
+            // Append text to path + a new line
+            File.AppendAllText(fileToAppendPath, text + Environment.NewLine);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Appendisso
+
+A plugin for [Wox](http://www.wox.one/) that appends your search to the end of a file.
+
+## Use
+
+As of now the file that is edited by the plugin is hardcoded, so you would need to compile it yourself and put the files on the Wox plugins folder.
+In the folder you need only
+plugin.js
+Appendisso.dll
+Images/
+
+## Why
+
+There are to-do applications that allow you to easily input tasks into the app. This plugin was thought to be used with plain-text todo files, like a todo.txt.
